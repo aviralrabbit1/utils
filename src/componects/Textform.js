2036d5fcc5c28beaa2b4fc1b9913ae0cbb696eb8
aviralrabbit1 <aviral.robin@gmail.com>
2023-06-03 18:18:06 +0530

    added copy clicoard and remove extra spaces button

--- src/componects/Textform.js
+++ src/componects/Textform.js
@@ -17,6 +17,18 @@ function Textform(props) {
     setText(text.toLowerCase());
     console.log("upperClick")
   }
+
+  const copyText = (text) => {
+    var text = document.getElementById("myBox");
+    text.select();
+    text.setSelectionRange(0,9999);
+    navigator.clipboard.writeText(text.value);
+  }
+
+  const removeExtraSpaces = () => {
+    setText(text.split(/[ ]+/).join(" "));
+  }
+
   const[text, setText] = useState('');
   // setText("New text"); //correct way to change the default set state
 
@@ -26,19 +38,23 @@ function Textform(props) {
       <h1>{props.heading}</h1>
         <div className="mb-3">
           <textarea className="form-control border-black" value={text} onChange={upperChange}
-          id="exampleFormControlTextarea1" rows="6">{props.heading} 
+          id="myBox" rows="6">{props.heading} 
           </textarea>
         </div>        
           <button className="btn btn-primary mx-1" onClick={upperClick}>Convert to Uppercase
           </button>
           <button className="btn btn-primary mx-1" onClick={lowerClick}>Convert to Lowercase
+          </button> 
+          <button className="btn btn-primary mx-1" onClick={copyText}>Copy to clipboard
+          </button>             
+          <button className="btn btn-primary mx-1" onClick={removeExtraSpaces}>Remove extra spaces
           </button>          
       </div>
       
       <div className="container my-3">
         <h2>Text Summary</h2>
         <p>{text.trim().split(" ").length} words {text.length} characters</p>
-        <p>{0.008*text.trim().split(" ").length} minutes read</p>
+        <p>{0.008*text.trim().split(/[ ]+/).length} minutes read</p>
         <h2>Preview</h2>
         <p>{text}</p>        
     </div>
